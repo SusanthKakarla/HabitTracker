e0b311ced74399823eea50716085bc6878a8c96c
Susanth Kakarla <51884959+SusanthKakarla@users.noreply.github.com>
2023-07-23 16:37:54 -0700

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Signitives on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
